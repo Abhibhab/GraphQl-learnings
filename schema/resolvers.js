39f6a9696455653448939b2cc4562a5cc1aa7156
Abhibhab <abhibhab2001@gmail.com>
2024-01-06 00:30:56 +0530

    first

---
+++ schema/resolvers.js
@@ -0,0 +1,27 @@
+const {UserList,MovieList}=require("../Fakedata");
+const _=require("lodash");
+const resolvers={
+    Query:{
+        users:()=>{
+            return UserList;
+        },
+   
+        user:(parent,args)=>{
+            const id=args.id;
+            const user=_.find(UserList,{id:Number(id)});
+            return user;
+
+        },
+        movies:()=>{
+            return MovieList;
+        },
+        movie:(parent,args)=>{
+            const name=args.name;
+            const movie=_.find(MovieList,{name});
+            return movie;            
+        
+        }
+        
+    }
+}
+module.exports={resolvers};
